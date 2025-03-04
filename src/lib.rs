db2673135b1abc806c99f019bf521863b8655ad2
duanjp8617 <duanjp8617@gmail.com>
2025-03-04 12:37:11 +0800

    backward pass works

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::collections::HashSet;
 use std::convert::From;
 use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
 use std::rc::Rc;
@@ -38,12 +39,16 @@ impl Value {
         self.0.borrow().gradient
     }
 
+    pub fn zero_grad(&self) {
+        self.0.borrow_mut().gradient = 0.0;
+    }
+
     pub fn powf(self, exponent: f64) -> Value {
         let x = self.0.clone();
         // d(x^exponent)/dx -> exponent * (x)^(exponent-1) * parent_grad
         let x_grad_func = Box::new(move |parent_grad: f64| {
-            x.borrow_mut().gradient +=
-                exponent * (x.borrow().data.powf(exponent - 1.0)) * parent_grad
+            let result = exponent * (x.borrow().data.powf(exponent - 1.0)) * parent_grad;
+            x.borrow_mut().gradient += result;
         });
 
         let val = ValueInner {
@@ -73,6 +78,33 @@ impl Value {
         };
 
         Value(Rc::new(RefCell::new(val)))
+    }
+
+    pub fn backward(&self) {
+        fn dfs(curr: ObjPtr, set: &mut HashSet<usize>, reverse_top: &mut Vec<ObjPtr>) {
+            for (n, _) in curr.borrow().prevs.iter() {
+                let node_idx = n.as_ptr() as usize;
+                if !set.contains(&node_idx) {
+                    set.insert(node_idx);
+                    dfs(n.clone(), set, reverse_top);
+                }
+            }
+            reverse_top.push(curr.clone());
+        }
+
+        let mut set = HashSet::new();
+        let mut reverse_top = Vec::new();
+        let curr = self.0.clone();
+        set.insert(curr.as_ptr() as usize);
+        dfs(curr, &mut set, &mut reverse_top);
+
+        reverse_top.last().unwrap().borrow_mut().gradient = 1.0;
+        for node in reverse_top.iter().rev() {
+            let node = node.borrow();
+            for (_, func) in node.prevs.iter() {
+                func(node.gradient);
+            }
+        }
     }
 }
 
@@ -245,5 +277,62 @@ mod tests {
 
         // println!("{:.4}", compute_graph());
         assert_eq!(compute_graph(), rust_native());
+    }
+
+    #[test]
+    fn backward_test() {
+        let compute_graph = || -> (f64, f64) {
+            let a = Value::new(-4.0);
+            let b = Value::new(2.0);
+            let mut c = a.clone() + b.clone();
+            let mut d = a.clone() * b.clone() + b.clone().powi(3);
+            c += c.clone() + 1.into();
+            c += Value::from(1) + c.clone() + (-a.clone());
+            d += d.clone() * 2.into() + (b.clone() + a.clone()).relu();
+            d += Value::from(3) * d.clone() + (b.clone() - a.clone()).relu();
+            let e = c.clone() - d.clone();
+            let f = e.clone().powi(2);
+            let mut g = f.clone() / 2.0.into();
+            g += Value::from(10.0) / f.clone();
+
+            g.backward();
+            (a.gradient(), b.gradient())
+        };
+
+        let _rust_native = || -> (f64, f64) {
+            let native_func = |a: f64, b: f64| -> f64 {
+                fn relu(n: f64) -> f64 {
+                    if n <= 0.0 {
+                        0.0
+                    } else {
+                        n
+                    }
+                }
+
+                let mut c = a + b;
+                let mut d = a * b + b.powi(3);
+                c += c + 1.0;
+                c += 1.0 + c + (-a);
+                d += d * 2.0 + relu(b + a);
+                d += 3.0 * d + relu(b - a);
+                let e = c - d;
+                let f = e.powi(2);
+                let mut g = f / 2.0;
+                g += 10.0 / f;
+                g
+            };
+
+            let manual_da =
+                |a: f64, b: f64, h: f64| -> f64 { (native_func(a + h, b) - native_func(a, b)) / h };
+
+            let manual_db =
+                |a: f64, b: f64, h: f64| -> f64 { (native_func(a, b + h) - native_func(a, b)) / h };
+
+            (manual_da(-4.0, 2.0, 0.0001), manual_db(-4.0, 2.0, 0.0001))
+        };
+
+        let (a, b) = compute_graph();
+        assert_eq!(format!("{:.4}", a), "138.8338");
+        assert_eq!(format!("{:.4}", b), "645.5773");
     }
 }
